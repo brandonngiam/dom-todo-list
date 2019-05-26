876888d5a0ade06b3366e26dbe9776d6a207986e
brandonngiam <brandonngiam@user.noreply.github.com>
2019-05-27 02:01:23 +0800

    Ready for submission.

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ toDoList.addEventListener("click", function(event) {
   event.target.classList.toggle("done");
 });
 
-//highlight in focus to-dos
+//highlight to-dos when mouse cursor is over it
 toDoList.addEventListener("mouseover", function(event) {
   //if statement prevents all li tags to be highlighted when mouse is over the
   //parent element
-  if (event.target !== event.currentTarget)
+  if (event.target.nodeName !== "UL")
     event.target.classList.add("todo-in-focus");
 });
 
@@ -31,14 +31,32 @@ toDoList.addEventListener("mouseout", function(event) {
   event.target.classList.remove("todo-in-focus");
 });
 
-//add new to do
+//User input
+addButton.addEventListener("click", function() {
+  if (
+    userInput.value.trim() !== "" &&
+    userInput.value.trim() !== "Add a new to-do"
+  )
+    addNewToDo(userInput.value);
+  userInput.value = "Add a new to-do";
+  userInput.classList.remove("userinput-in-focus");
+});
 
-// userInput.addEventListener("click", function(event) {
-//   event.currentTarget.value = "";
-//   event.target.classList.add(".userinput-in-focus");
-// });
+userInput.addEventListener("keydown", function(event) {
+  if (event.key === "Enter" && userInput.value.trim() !== "") {
+    addNewToDo(userInput.value);
+    userInput.value = "";
+  }
+});
 
-// addButton.addEventListener("click", function() {
-//   if (userInput.value !== "") addNewToDo(userInput.value);
-//   userInput.value = "";
-// });
+userInput.addEventListener("focus", function(event) {
+  event.currentTarget.value = "";
+  event.target.classList.add("userinput-in-focus");
+});
+
+userInput.addEventListener("focusout", function(event) {
+  if (event.currentTarget.value.trim() === "") {
+    event.currentTarget.value = "Add a new to-do";
+    event.target.classList.remove("userinput-in-focus");
+  }
+});
